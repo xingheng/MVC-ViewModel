a83d5846e229d64b40911db7e502043e3503a1c6
Wei Han <xingheng907@hotmail.com>
2022-06-06 17:23:53 +0800

    Initial Commit

---
+++ MVC-ViewModel/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MVC-ViewModel
+//
+//  Created by WeiHan on 2022/6/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVC-ViewModel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVC-ViewModel
+//
+//  Created by WeiHan on 2022/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
